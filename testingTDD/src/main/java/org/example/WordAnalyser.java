73df10f242096ffd1725f0bfaf55c36a1480d113
John McTigue <m.godotishere@gmail.com>
2024-04-30 20:19:59 +0100

    Challenge 08 Initial code skeleton

---
+++ testingTDD/src/main/java/org/example/WordAnalyser.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class WordAnalyser {
+    import java.util.Map;
+
+    public class WordAnalyser {
+        public String[] findLongestWords(String text) {
+            // TODO: Implement the logic to find the longest word(s) in the given text
+            return new String[]{""};
+        }
+
+        public Map<Character, Integer> calculateLetterFrequency(String text) {
+            // TODO: Implement the logic to calculate the frequency of each letter in the given text
+            return null;
+        }
+    }
+}
