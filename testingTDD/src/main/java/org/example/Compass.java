adf3f95300da6137fb8397fd086b7d37d643dee1
John McTigue <m.godotishere@gmail.com>
2024-04-30 15:25:49 +0100

    Challenge 06a

--- testingTDD/src/main/java/org/example/Compass.java
+++ testingTDD/src/main/java/org/example/Compass.java
@@ -26,16 +26,16 @@ public class Compass {
             switch (point) {
                 case NORTH:
                     return Point.WEST;
-                break;
+
                 case SOUTH:
                     return Point.EAST;
-                break;
+
                 case EAST:
                     return Point.NORTH;
-                break;
+
                 case WEST:
                     return Point.SOUTH;
-                break;
+
                 default:
                     break;
             }
@@ -43,19 +43,20 @@ public class Compass {
             switch (point) {
                 case NORTH:
                     return Point.EAST;
-                    break;
+
                 case SOUTH:
                     return Point.WEST;
-                    break;
+
                 case EAST:
                     return Point.SOUTH;
-                    break;
+
                 case WEST:
                     return Point.NORTH;
-                    break;
+
                 default:
                     break;
             }
         }
+        return point;
     }
 }
